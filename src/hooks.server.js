3fb5b27c9e58fa8cdd3e6ca10bc0175cc32fb045
Apollo <apollomacabodbod8@gmail.com>
2025-02-28 09:07:37 +0800

    added logger and authorize hooks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,10 +1,25 @@
-
+import { sequence } from '@sveltejs/kit/hooks';
 import { auth } from '$db/fake_auth'
 
-export async function handle({ event, resolve }){
+
+async function logger({ event, resolve }) {
+    const start_time = Date.now()
+
+     console.log('start_time', start_time)
+    const response = await resolve(event)
+
+    console.log(`${Date.now() -start_time}ms ${event.request.method} ${event.url.pathname}`)
+    return response
+}
+
+
+function authorize ({ event, resolve }){
+
+    console.log(`Hi`)
+
     const user = auth() 
     event.locals.user = user;
-    
     return resolve(event)
-
 }
+
+export const handle = sequence(logger,authorize)
--- src/routes/show/[num]/+page.server.js
+++ src/routes/show/[num]/+page.server.js
@@ -2,7 +2,6 @@
 
 
 export async function load({ fetch, params , setHeaders , locals }){
-    console.log("locals: ", locals)
 
     const res = await fetch(`https://syntax.fm/api/shows/${params.num}`)
     const data = await res.json();
